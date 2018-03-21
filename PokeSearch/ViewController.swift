29d1a87fd6e9761fc8c39ef2eac7948bcbc49787
Aleksei Degtiarev <delexa0@gmail.com>
2018-03-21 22:59:33 +0100

    Configured mapView

--- PokeSearch/ViewController.swift
+++ PokeSearch/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
+    var mapHasCenteredOnce = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,21 +24,49 @@ class ViewController: UIViewController, MKMapViewDelegate {
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        locationAuthStatus()
+    }
+    
+    
+    
+    func locationAuthStatus() {
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
             mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
         }
-        
     }
     
-    
-    
-    func loacationAuthStatus() {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         
+        if status == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        
+        if let loc = userLocation.location {
+            if !mapHasCenteredOnce {
+                centerMapOnLocation(location: loc)
+                mapHasCenteredOnce = true
+            }
+        }
+    } //  func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView: MKAnnotationView?
+        if annotation.isKind(of: MKUserLocation.self) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named: "ash")
+        }
+        return annotationView
     }
     
     @IBAction func spotRandomPokemon(_ sender: Any) {
